da96e2d973df654ee6d94ea09fe351f5abb932f6
kthurm <karalthurmond@gmail.com>
2023-11-16 09:17:31 -0500

    adds nav items to main nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Link } from "react-scroll";
 import { HiMenuAlt3, HiX } from "react-icons/hi";
 
-import NavBarItems from "./navBarItems";
 import SocialSideNav from "./SocialSideNav";
 
 function NavBar() {
@@ -21,9 +20,6 @@ function NavBar() {
         >
           KT
         </Link>
-        <ul className="hidden md:flex space-x-5 uppercase text-sm">
-          <NavBarItems />
-        </ul>
 
         {/* hambuger */}
         <div onClick={handleClick} className="md:hidden">
@@ -33,16 +29,66 @@ function NavBar() {
         <div
           className={
             !nav
-              ? "hidden"
-              : "md:hidden absolute top-[40px] right-0 bg-light text-dark shadow-2xl px-5 py-3 rounded-b-lg text-right"
+              ? "hidden md:flex"
+              : "md:relative absolute top-[40px] right-0 bg-light text-dark shadow-2xl px-5 py-3 rounded-b-lg text-right"
           }
         >
-          <ul className="flex flex-col space-y-5 uppercase text-sm">
-            <NavBarItems />
+          <ul className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 uppercase text-sm">
+            <li>
+              <Link
+                activeClass="active"
+                to="home"
+                smooth={true}
+                offset={-90}
+                duration={500}
+                onClick={handleClick}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                activeClass="active"
+                to="skills"
+                smooth={true}
+                offset={-60}
+                duration={500}
+                onClick={handleClick}
+              >
+                Skills
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                activeClass="active"
+                to="work"
+                smooth={true}
+                offset={-60}
+                duration={500}
+                onClick={handleClick}
+              >
+                Work
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                activeClass="active"
+                to="contact"
+                smooth={true}
+                offset={-60}
+                duration={500}
+                onClick={handleClick}
+              >
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
-      <SocialSideNav />
+      <SocialSideNav className="hidden md:flex" />
     </div>
   );
 }
