25a07c7194d3cd851184a4b788100d82b7fc0701
kthurm <karalthurmond@gmail.com>
2023-11-16 09:18:45 -0500

    adds sm icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import SocialSideNav from "./SocialSideNav";
 
 const Footer = () => {
   return (
-    <div className="bg-dark/80 lg:ml-auto py-7 px-5 mt-5 text-light/50 uppercase text-xs">
-      @ Kara Thurmond
+    <div className="bg-dark/80 flex items-center justify-between lg:flex-row-reverse">
+      <div className="py-7 px-5 lg:mt-5 text-light/50 uppercase text-xs">
+        @ Kara Thurmond
+      </div>
+      <SocialSideNav className="md:hidden" />
     </div>
   );
 };
